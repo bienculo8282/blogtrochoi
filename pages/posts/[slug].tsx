144b08aae6b293bba43039c25a7fa732314ec33f
tuanbia <139607434+tuanbia@users.noreply.github.com>
2023-10-15 00:59:13 +0700

    Update [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -46,6 +46,7 @@ export default function Post({ post, posts, preview }) {
                 </script>
 
               </Head>
+              <AdsMgidHeader />
               <PostHeader
                 title={post.title}
                 coverImage={post.featuredImage}
@@ -53,7 +54,6 @@ export default function Post({ post, posts, preview }) {
                 author={post.author}
                 categories={post.categories}           
               />
-              <AdsMgidHeader />
               <PostBody content={post.content}
               />
               
