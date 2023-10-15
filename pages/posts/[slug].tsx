27145777c22c5aa273ca73bc0b1f810143c6c239
bienculo8282 <139576246+bienculo8282@users.noreply.github.com>
2023-10-15 11:48:35 -0400

    Update [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -35,16 +35,14 @@ const lastMGID = `<!-- Composite Start -->
 </script>
 <!-- Composite End -->`;
 
-const autoGA = `	<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-45012531-1"></script>
+const autoGA = `<script async src="https://www.googletagmanager.com/gtag/js?id=UA-45012531-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'UA-45012531-1');
 </script>
-	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2271746353762763"
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2271746353762763"
      crossorigin="anonymous"></script>`;
 
 export default function Post({ post, posts, preview }) {
